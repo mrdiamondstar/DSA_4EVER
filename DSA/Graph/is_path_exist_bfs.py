89d0a23d7aad8f4d624aabbe83828c49e6187a40
mrdiamondstar <deepakhadimani02@gmail.com>
2026-05-19 18:59:13 +0530

    bfs

---
+++ DSA/Graph/is_path_exist_bfs.py
@@ -0,0 +1,19 @@
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        neigh = defaultdict(list)
+        for n1, n2 in edges:
+            neigh[n1].append(n2)
+            neigh[n2].append(n1)
+        q = deque()
+        q.append(source)
+        seen = set()
+        seen.add(source)
+        while q:
+            node = q.popleft()
+            if node == destination:
+                return True
+            for n in neigh[node]:
+                if n not in seen:
+                    seen.add(n)
+                    q.append(n)
+        return False
